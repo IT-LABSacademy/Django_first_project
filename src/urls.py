5e2e981dfaa5a5d84b4e520a896e2aefc9309c62
BunyodIsmailov <ishonch055@mail.ru>
2023-06-16 17:34:09 +0500

    dep

--- src/urls.py
+++ src/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from my_app import views
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +28,6 @@ urlpatterns = [
     path('sport/', views.sp_page, name='sport'),
     
 ]
+
+# urlpatterns+=static(settings.MEDIA_URL,
+#                     document_root=settings.MEDIA_ROOT)
